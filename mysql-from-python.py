3a36555d4f3a34197e92ee20c8d4f9d654f8c7b8
Kes Cardoso <kesia.development@gmail.com>
2020-03-20 19:43:57 +0000

    Update files

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -11,10 +11,9 @@ connection = pymysql.connect(
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ('bob','jim')")
+        connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
